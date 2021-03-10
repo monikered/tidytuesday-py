360f2d1a10f76fd383cdf726106373545b1c39b3
monikered <monica.remmers@gmail.com>
2021-03-09 22:06:42 -0600

    added cleaning script

--- code/2021_11_bechdeltest.py
+++ code/2021_11_bechdeltest.py
@@ -5,3 +5,24 @@ Created on Mon Mar  8 17:55:09 2021
 
 @author: monicaremmers
 """
+
+import pandas as pd
+import re
+
+
+movies = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv', error_bad_lines=False)
+
+# clean up names of films in dataset
+def Clean_names(title):    
+    title = re.sub('&amp;', '&', title)
+    title = re.sub('&#39;', '\'', title)
+    title = re.sub('&uuml;', 'ü', title)
+    title = re.sub('&agrave;', 'à', title)
+    return title
+
+movies['title'] = movies['title'].apply(Clean_names)
+
+# Which directors have the most films on this list?
+directors = movies.groupby('director').count().reset_index()
+
+# MPAA ratings and bechdel test
